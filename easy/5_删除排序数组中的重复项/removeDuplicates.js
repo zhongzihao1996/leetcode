ad25a19d5e5dab643df5af3ae746f7ba9e1fcbdf
zhongzihao1996 <zhongzihao@ijunhai.com>
2020-05-25 18:01:50 +0800

    删除数组重复项

---
+++ easy/5_删除排序数组中的重复项/removeDuplicates.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  if (nums.length === 0) return 0;
+  var i = 0;
+  for (let j = 1, len = nums.length; j < len; j++) {
+    if (nums[j] != nums[i]) {
+      i++;
+      nums[i] = nums[j];
+    }
+  }
+  return i + 1;
+}
+
+console.log(removeDuplicates([1, 1, 2]));

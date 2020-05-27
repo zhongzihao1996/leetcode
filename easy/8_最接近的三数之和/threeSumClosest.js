79ed3f3c314ae1672807efeb44fed6ba00f6853d
zhongzihao1996 <zhongzihao@ijunhai.com>
2020-05-27 11:35:10 +0800

    8_最接近的三数之和

---
+++ easy/8_最接近的三数之和/threeSumClosest.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function (nums, target) {
+  nums.sort((a, b) => a - b); // 先排好序
+  let result = nums[0] + nums[1] + nums[2];
+  for (let i = 0, len = nums.length; i < len; i++) {
+    let left = i + 1;
+    let right = len - 1;
+    // 直到左右两指针相遇
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+      if (Math.abs(target - result) > Math.abs(target - sum)) {
+        result = sum;
+      }
+      // 已经排好序的，若比目标值大，则移动右指针
+      if (sum > target) {
+        right--;
+      } else {
+        left++;
+      }
+    }
+  }
+  return result;
+};
+
+console.log(threeSumClosest([-1, 2, 1, -4], 1));

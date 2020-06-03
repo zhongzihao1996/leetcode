8f3ccc1d05b9c05a59ef70cef43272a7644bd9e1
zhongzihao1996 <zhongzihao@ijunhai.com>
2020-06-03 17:40:41 +0800

    6_猜数字

---
+++ easy/6_猜数字/game.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} guess
+ * @param {number[]} answer
+ * @return {number}
+ */
+var game = function (guess, answer) {
+  let count = 0;
+  for (let i = 0; i < guess.length; i++) {
+    if (guess[i] === answer[i]) {
+      count++;
+    }
+  }
+  return count;
+};
+
+console.log(game([1, 2, 3], [1, 2, 3]));

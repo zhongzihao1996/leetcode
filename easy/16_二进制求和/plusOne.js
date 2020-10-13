02ff42111193fcb713aa281d6376bde2897e0529
zhongzihao1996 <zhongzihao@ijunhai.com>
2020-10-13 11:39:32 +0800

    16_二进制求和

--- easy/16_二进制求和/plusOne.js
+++ easy/16_二进制求和/plusOne.js
@@ -4,6 +4,7 @@
  * @return {string}
  */
 var addBinary = function (a, b) {
+  // 先补齐位数
   let abs_len = Math.abs(a.length - b.length);
   while (abs_len > 0) {
     if (a.length > b.length) {
@@ -14,13 +15,20 @@ var addBinary = function (a, b) {
     abs_len--;
   }
   let c = '';
-  let end = a.length - 1;
+  // 进位 0  或者  1
   let over_byte = 0;
+  // 从尾部开始遍历
+  // 每次遍历后都重置进位
+  let end = a.length - 1;
   while (end >= 0) {
+    // 位数相加并且加上进位
     let count = Number(a[end]) + Number(b[end]) + over_byte;
+    // 有进位
     if (count >= 2) {
+      // 取模
       c = String(count % 2) + c;
       over_byte = 1;
+      // 处理最后一位
       if (end === 0) {
         c = '1' + c;
       }

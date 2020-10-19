b14a9785bbf4e90d448c8344d4c5dd1cf08eec35
zhongzihao1996 <zhongzihao@ijunhai.com>
2020-10-19 10:37:28 +0800

    6_两数相加

---
+++ medium/6_两数相加/addTwoNumbers.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+// JS没有链表结构
+// 因为是逆序，所以可以直接末尾相加，保存进位，若有一个链表完了，结束，末尾进位需要注意
+var addTwoNumbers = function (l1, l2) {
+  let head = null,
+    tail = null;
+  let carry = 0;
+  while (l1 || l2) {
+    const n1 = l1 ? l1.val : 0;
+    const n2 = l2 ? l2.val : 0;
+    const sum = n1 + n2 + carry;
+    if (!head) {
+      head = tail = new ListNode(sum % 10);
+    } else {
+      tail.next = new ListNode(sum % 10);
+      tail = tail.next;
+    }
+    carry = Math.floor(sum / 10);
+    if (l1) {
+      l1 = l1.next;
+    }
+    if (l2) {
+      l2 = l2.next;
+    }
+  }
+  if (carry > 0) {
+    tail.next = new ListNode(carry);
+  }
+  return head;
+};

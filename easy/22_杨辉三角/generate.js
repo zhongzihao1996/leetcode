084e1da5f3209050d7d2df22271a7ab88c85af32
zhongzihao1996 <zhongzihao@ijunhai.com>
2021-01-27 10:40:45 +0800

    22_杨辉三角

--- easy/22_杨辉三角/generate.js
+++ easy/22_杨辉三角/generate.js
@@ -5,23 +5,15 @@
 var generate = function (numRows) {
   if (numRows === 1) return [[1]];
   if (numRows === 2) return [[1], [1, 1]];
-  let result = [];
+  const result = [];
   for (let i = 0; i < numRows; i++) {
-    let temp_list = [];
-    if (i === 0) {
-      temp_list = [1];
-    } else if (i === 1) {
-      temp_list = [1, 1];
-    } else {
-      temp_list = new Array(i + 1);
-      for (let j = 0; j < temp_list.length; j++) {
-        if (j === 0 || j === temp_list.length - 1) {
-          temp_list[j] = 1;
-        } else {
-          temp_list[j] = result[i - 1][j - 1] + result[i - 1][j];
-        }
+    // 前两行都为1
+    let temp_list = new Array(i + 1).fill(1);
+    // 第三行开始
+    for (let j = 1; j < i; j++) {
+      if (j !== 0 || j !== i) {
+        temp_list[j] = result[i - 1][j - 1] + result[i - 1][j];
       }
-      console.log(temp_list);
     }
     result.push(temp_list);
   }
